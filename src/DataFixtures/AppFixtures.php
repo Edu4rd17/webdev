3876f400e8f338064dfc393161a9c08ca0cbf1c5
Eduard Iacob <B00125295@mytudublin.ie>
2022-03-30 22:04:55 +0100

    Product factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -31,6 +32,27 @@ class AppFixtures extends Fixture
             'email' => 'Eduard@gmail.com',
             'phone' => '646356456456',
             'role' => 'ROLE_USER'
+        ]);
+
+        ProductFactory::createOne([
+            'product_name' => 'Steak',
+            'product_price' => 14.99,
+            'is_available'=> true,
+            'product_description' => 'Delicious steak with vegetables.'
+        ]);
+
+        ProductFactory::createOne([
+            'product_name' => 'Pancakes',
+            'product_price' => 6.99,
+            'is_available'=> true,
+            'product_description' => 'Delicious steak with vegetables.'
+        ]);
+
+        ProductFactory::createOne([
+            'product_name' => 'Soup',
+            'product_price' => 4.99,
+            'is_available'=> false,
+            'product_description' => 'Delicious steak with vegetables.'
         ]);
 
     }
