f533725acd8aa34c89a32316c35498e27fb40bcd
Eduard Iacob <B00125295@mytudublin.ie>
2022-05-03 00:08:26 +0100

    //

---
+++ migrations/Version20220502213939.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220502213939 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, number_people_id INT DEFAULT NULL, date DATE NOT NULL, time TIME NOT NULL, INDEX IDX_E00CEDDE1CC5344E (number_people_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE1CC5344E FOREIGN KEY (number_people_id) REFERENCES `table` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE booking');
+    }
+}
